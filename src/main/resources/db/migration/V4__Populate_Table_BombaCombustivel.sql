59a2663670355a01883dd95a6d8b0947e6a0a322
marliseborba <borba.marlise@gmail.com>
2025-11-16 21:28:35 -0300

    Atualiza Migrations

--- src/main/resources/db/migration/V4__Populate_Table_BombaCombustivel.sql
+++ src/main/resources/db/migration/V4__Populate_Table_BombaCombustivel.sql
@@ -1,9 +1,22 @@
 INSERT INTO `bombas_combustivel` (nome,tipo_combustivel_id) VALUES
 	 ('Bomba de Gasolina Comum',1),
 	 ('Bomba de Gasolina Comum',1),
+	 ('Bomba de Gasolina Comum',1),
+	 ('Bomba de Gasolina Comum',1),
+	 ('Bomba de Gasolina Comum',1),
 	 ('Bomba de Gasolina Aditivada',2),
 	 ('Bomba de Gasolina Aditivada',2),
+	 ('Bomba de Gasolina Aditivada',2),
+	 ('Bomba de Gasolina Aditivada',2),
+	 ('Bomba de Gasolina Aditivada',2),
+	 ('Bomba de Etanol',3),
+	 ('Bomba de Etanol',3),
+	 ('Bomba de Etanol',3),
 	 ('Bomba de Etanol',3),
 	 ('Bomba de Etanol',3),
 	 ('Bomba de Diesel',4),
+	 ('Bomba de Diesel',4),
+	 ('Bomba de Diesel',4),
+	 ('Bomba de Diesel',4),
 	 ('Bomba de Diesel',4);
+
